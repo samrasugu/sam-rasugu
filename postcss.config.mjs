7e83a0d0c5b357024011e3228c6c1ed8cdde4bb4
Sam Rasugu <mokuasamr@gmail.com>
2025-04-09 18:14:20 +0300

    chore: upgrade NextJS to 15.2.5

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 const config = {
   plugins: [
-    "@tailwindcss/postcss",
     "postcss-import", // for @ imports in css files
+    "@tailwindcss/postcss",
     "autoprefixer", // for autoprefixing css -- better browser compatibility
     ...(process.env.NODE_ENV === "production" ? ["cssnano"] : []), // minify only in production
   ],
