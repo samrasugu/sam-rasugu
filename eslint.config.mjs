f7c82e446eda2532b8002fd255d61a84ce8ff5f2
Sam Rasugu <mokuasamr@gmail.com>
2025-05-16 13:30:29 +0300

    fix: eslint prettier plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,14 +12,24 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    env: { node: true, browser: true, es6: true },
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+      "eslint:recommended",
+      "prettier",
+      "plugin:prettier/recommended",
+      "@studio/eslint-config-studio",
+    ],
     plugins: ["prettier"],
     rules: {
       "no-console": "warn",
       "prettier/prettier": "error",
     },
-  },
+  }),
+  eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
